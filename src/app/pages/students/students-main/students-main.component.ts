1844d733ce28ad179d92bf43db72eda0e67bde6b
João Clemer <joaoclemerlima@gmail.com>
2024-03-30 01:52:25 -0300

    feat: add pagination and filter options in layout

--- src/app/pages/students/students-main/students-main.component.ts
+++ src/app/pages/students/students-main/students-main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InfoCardComponent } from '../../../components/info-card/info-card.component';
+import { PrimaryInputComponent } from '../../../components/primary-input/primary-input.component';
 
 @Component({
   selector: 'app-students-main',
   standalone: true,
-  imports: [InfoCardComponent],
+  imports: [InfoCardComponent, PrimaryInputComponent],
   templateUrl: './students-main.component.html',
   styleUrl: './students-main.component.scss'
 })
